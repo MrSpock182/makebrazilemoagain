568d69204eb158e184dd8e80664356784d348be4
Kleber Nunes <mrspock182@MacBook-Pro-de-Kleber.local>
2020-03-27 15:12:38 -0300

    origin

--- src/main/java/br/com/studiotrek/makebrazilemoagain/api/PodcastAPI.java
+++ src/main/java/br/com/studiotrek/makebrazilemoagain/api/PodcastAPI.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PodcastAPI {
 
     @Autowired
